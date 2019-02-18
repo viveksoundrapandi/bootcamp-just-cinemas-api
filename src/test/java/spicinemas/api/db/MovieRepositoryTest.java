27e8bbdbf1a881783c0ac6818dac42f345be9188
abistarun <abistarun@gmail.com>
2019-02-18 19:09:06 +0530

    Corrected Test Cases after adding DB Scripts

--- src/test/java/spicinemas/api/db/MovieRepositoryTest.java
+++ src/test/java/spicinemas/api/db/MovieRepositoryTest.java
@@ -30,8 +30,8 @@ public class MovieRepositoryTest {
 
     @Test
     public void shouldInsertUserInDb(){
-        String movieName = "Infinity War";
-        Movie expectedMovie = new Movie(movieName, "okay", MovieListingType.NOW_SHOWING);
+        String movieName = "Infinity War 1";
+        Movie expectedMovie = new Movie(movieName, "RDX, Dolby Atmos, SUB", MovieListingType.NOW_SHOWING);
         movieRepo.addMovie(expectedMovie);
         Movie actualMovie = movieRepo.getMovie(movieName);
         assertThat(actualMovie.getName(), is(expectedMovie.getName()));
@@ -42,18 +42,18 @@ public class MovieRepositoryTest {
     @Test
     public void shouldReturnAllMoviesWithListingTypeNowShowing(){
 
-        Movie movie1 = new Movie("Infinity War", "okay", MovieListingType.NOW_SHOWING);
-        Movie movie2 = new Movie("Iron Man", "okay", MovieListingType.UPCOMING);
+        Movie movie1 = new Movie("Infinity War", "RDX, Dolby Atmos, SUB", MovieListingType.NOW_SHOWING);
+        Movie movie2 = new Movie("Iron Man", "RDX, Dolby Atmos, SUB", MovieListingType.UPCOMING);
         movieRepo.addMovie(movie1);
         movieRepo.addMovie(movie2);
         List<Movie> movies = movieRepo.getNowShowingMovies();
-        Assert.assertEquals("Total now showing movies should be 1",1, movies.size());
+        Assert.assertEquals("Total now showing movies should be 1",11, movies.size());
     }
 
     @Test
     public void shouldReturnAMovieByName(){
-        Movie movie1 = new Movie("Infinity War", "okay", MovieListingType.NOW_SHOWING);
-        Movie movie2 = new Movie("Iron Man", "okay", MovieListingType.UPCOMING);
+        Movie movie1 = new Movie("Infinity War", "RDX, Dolby Atmos, SUB", MovieListingType.NOW_SHOWING);
+        Movie movie2 = new Movie("Iron Man", "RDX, Dolby Atmos, SUB", MovieListingType.UPCOMING);
         movieRepo.addMovie(movie1);
         movieRepo.addMovie(movie2);
         Movie movie = movieRepo.getMovie("Iron Man");
